35a04c034afe77c31440bd3828364bbab8f84a5f
AtharvaDahake <131685891+AtharvaDahake@users.noreply.github.com>
2026-09-24 15:19:46 +0530

    Add files via upload

---
+++ GENAI/2_Chat_Models/5_chatmodel_hf_local.py
@@ -0,0 +1,19 @@
+# dont try this at home 
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+
+
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='meta-llama/Llama-3.1-8B-Instruct',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
